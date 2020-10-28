faa18cf22ead1defe09427c33bf6a5e313048e86
munozjuan <munoz.juan07@hotmail.com>
2020-10-27 23:14:22 -0400

    added some else if to function of passwordLenght

--- 03-JavaScript_02-Homework_Develop_script.js
+++ 03-JavaScript_02-Homework_Develop_script.js
@@ -15,14 +15,28 @@ var checkSpecial;
 function chooseLenght(){
   passwordLength = prompt("How many characters would you like your password to be? (between 8-128 characters)  ");
 
-}
+  //if statement to check length is between 8-128 
+  //first I must check it should not be less than 8
+  if (passwordLength<8){
+    alert("Password needs to be 8-128 characters!"); //this alerts if it's below 8 chars
+    chooseLenght();
 
 
+  }else if(passwordLength>128){
+      alert("Password needs to be 8-128 characters!");
+      chooseLenght();
+    }
+    return passwordLength;
+
+
+  }
+
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
