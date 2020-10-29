a0c3631e2450213fb01f6a9259a58e5fe7fafad0
munozjuan <munoz.juan07@hotmail.com>
2020-10-28 22:39:45 -0400

    notated on code

--- 03-JavaScript_02-Homework_Develop_script.js
+++ 03-JavaScript_02-Homework_Develop_script.js
@@ -21,21 +21,25 @@ function chooseLenght(){
     alert("Password needs to be 8-128 characters!"); //this alerts if it's below 8 chars
     chooseLenght();
 
-
+//makd sure password is less than 128 chars
   }else if(passwordLength>128){
       alert("Password needs to be 8-128 characters!");
       chooseLenght();
+      //if they do not choose any leght this will display aka not a number
+    }else if (isNaN(passwordLength)){
+      alert("Password needs to be 8-128 characters!");
+      determineLength();
+    }else{
+    alert("The next screens will ask you what types of characters you want in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
     }
     return passwordLength;
-
-
   }
 
   
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  
+
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
